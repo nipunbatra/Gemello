0bf712ff398be9121c68421dbb2a35f50adab704
Nipun <nipunb@iiitd.ac.in>
2016-02-07 23:36:08 +0530

    corrected JSON writing

--- code/sensitivity_num_features.py
+++ code/sensitivity_num_features.py
@@ -26,5 +26,5 @@ out_mean, optimal_dict_mean = find_optimal_features(df, dfc, all_homes, applianc
                             print_steps=False)
 
 import json
-json.dump(optimal_dict_mean, open("%s_%s" %(appliance, feature), 'w'))
+json.dump(optimal_dict_mean, open("../sensitivity-data/%s_%s.json" %(appliance, feature), 'w'))
 
