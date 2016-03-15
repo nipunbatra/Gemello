c0646e409a163827ade09758c7c908b94325bd70
Nipun Batra <nipunb@iiitd.ac.in>
2016-03-15 18:09:00 +0530

    Added cross region cluster

---
+++ cluster/cross_region_cluster.py
@@ -0,0 +1,50 @@
+appliances = ["hvac"]
+features = ["Monthly"]
+strategy_list = ["None","CDD"]
+
+import sys
+CLUSTER=True
+if CLUSTER:
+    sys.path.insert(0, '/if6/nb2cz/anaconda/lib/python2.7/site-packages')
+
+sys.path.insert(0, '../new_experiments')
+
+from train_test import *
+
+
+
+import numpy as np
+import pandas as pd
+
+
+train_region = "SanDiego"
+test_region = "Austin"
+
+test_all_homes = home_regions[test_region]
+
+
+SLURM_OUT = "../slurm_out"
+from subprocess import Popen
+
+
+for feature in features:
+    for appliance in appliances:
+        for home in test_all_homes[appliance]:
+            for strategy in strategy_list:
+
+                OFILE = "%s/%d_%s_%s.out" % (SLURM_OUT, home, appliance, feature)
+                EFILE = "%s/%d_%s_%s.err" % (SLURM_OUT, home, appliance,  feature)
+                SLURM_SCRIPT = "%d_%s_%s.pbs" % (home, appliance,feature)
+                CMD = 'python ../new_experiments/cross_region %s %s %s %s %d %s' % (train_region, test_region, appliance, strategy, home, feature)
+                lines = []
+                lines.append("#!/bin/sh\n")
+                lines.append('#SBATCH --time=0-06:0:00\n')
+                lines.append('#SBATCH --mem=16\n')
+                lines.append('#SBATCH -o '+'"' +OFILE+'"\n')
+                lines.append('#SBATCH -e '+'"' +EFILE+'"\n')
+                lines.append(CMD+'\n')
+
+                with open(SLURM_SCRIPT, 'w') as f:
+                   f.writelines(lines)
+                command = ['sbatch', SLURM_SCRIPT]
+                print Popen(command)
