77c0dad3b2662cdfbf194d4af59986c0388878ab
Nipun Batra <nipunb@iiitd.ac.in>
2016-03-29 12:30:10 +0530

    Added more things to cluster pred

--- cluster/cluster_pred_ineq.py
+++ cluster/cluster_pred_ineq.py
@@ -13,9 +13,12 @@ K = 3
 
 train_df = out_overall[train_region]
 test_df = out_overall[test_region]
-
+total = len(test_df)
 for transform in ["DD","None"]:
+    count = -1
     for test_home in test_df.index[:]:
+        count+= 1
+        print test_home, count,total
         for appliance in ["hvac","fridge","wm"]:
             for month in range(1, 13):
                 OFILE = "%s/%s_%s_%d_%s_%d_%s.out" % (SLURM_OUT, train_region, test_region, test_home, appliance, month, transform )
