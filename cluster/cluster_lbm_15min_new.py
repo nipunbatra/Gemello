2176643d3f09255ed8e35ab1615da64d06a68b71
Nipun Batra <nipunb@iiitd.ac.in>
2016-03-30 08:56:53 +0530

    lesser gap now..

--- cluster/cluster_lbm_15min_new.py
+++ cluster/cluster_lbm_15min_new.py
@@ -2,7 +2,7 @@ SLURM_OUT = "../slurm_out"
 from subprocess import Popen
 import time
 
-for NUM_HOMES in range(5, 628):
+for NUM_HOMES in range(398, 628):
 
     for DAY in range(1, 365):
 
@@ -27,4 +27,4 @@ for NUM_HOMES in range(5, 628):
     print NUM_HOMES
     print "*"*40
     print "Now sleeping.."
-    time.sleep(90)
+    time.sleep(40)
