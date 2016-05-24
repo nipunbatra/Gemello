a72e56e5f5f174998ce8fbf9ea335cb8c50c0c32
Nipun Batra <nipunb@iiitd.ac.in>
2016-05-24 16:19:49 +0530

    added reducing cod

---
+++ new_experiments/reduce_results_subset_homes_home_number_error_cluster.py
@@ -0,0 +1,62 @@
+appliances = ["hvac"]
+features = ["Static","Monthly","Monthly+Static"]
+features=["Monthly","Monthly+Static","Static"]
+appliances=["hvac","fridge"]
+SLURM_OUT = "../slurm_out"
+from subprocess import Popen
+
+import  os
+
+import numpy as np
+import pandas as pd
+
+import sys
+sys.path.append("../code")
+
+
+from create_df_larger import read_df_larger
+df, dfc, all_homes, appliance_min, national_average = read_df_larger()
+print ("Read DF")
+
+df = df.rename(columns={'house_num_rooms':'num_rooms',
+                        'num_occupants':'total_occupants',
+                        'difference_ratio_min_max':'ratio_difference_min_max'})
+K_min, K_max = 1,6
+F_min, F_max=1,8
+
+from all_functions import *
+from features_larger import *
+
+print ("All functions done")
+out ={}
+for num_homes in range(6, 50, 5):
+    out[num_homes] = {}
+    for feature in features:
+        out[num_homes][feature] = {}
+        for appliance in appliances:
+            out[num_homes][feature][appliance]={}
+            appliance_df = df.ix[all_homes[appliance]]
+            if appliance=="hvac":
+                start, stop=5, 11
+            else:
+                start, stop=1, 13
+
+            for home in appliance_df.index.values:
+                try:
+                    store_path = os.path.expanduser("~/main-out-new-larger-num-homes/%d_%s_%s_%d.csv" %(num_homes, appliance,feature, home))
+                    pred =  pd.read_csv(store_path, index_col=0).squeeze()
+                    gt = appliance_df.ix[home][['%s_%d' %(appliance, month) for month in range(start, stop)]].squeeze()
+                    gt.index = pred.index
+                    #print gt
+                    error = (gt-pred).abs()
+                    #print error
+                    percentage_error = error*100/gt
+                    #print percentage_error
+                    percentage_error[percentage_error>100]=100
+
+
+                    percentage_accuracy = 100-percentage_error
+                    #print error.mean()
+                    out[num_homes][feature][appliance][home] = error.mean()
+                except Exception, e:
+                    pass
