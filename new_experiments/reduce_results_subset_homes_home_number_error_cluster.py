f4718edc27e0f4f1c91038192dad2e998fdc9bcd
Nipun Batra <nipunb@iiitd.ac.in>
2016-05-25 16:27:44 +0530

    now trying to compare against data set mean

--- new_experiments/reduce_results_subset_homes_home_number_error_cluster.py
+++ new_experiments/reduce_results_subset_homes_home_number_error_cluster.py
@@ -2,6 +2,7 @@ appliances = ["hvac"]
 features = ["Static","Monthly","Monthly+Static"]
 features=["Monthly","Monthly+Static","Static"]
 appliances=["hvac","fridge","wm"]
+appliance=["hvac","fridge"]
 SLURM_OUT = "../slurm_out"
 from subprocess import Popen
 
@@ -29,12 +30,16 @@ from features_larger import *
 
 print ("All functions done")
 out ={}
+out_mean = {}
 for num_homes in range(6, 50, 5):
     out[num_homes] = {}
+    out_mean[num_homes] ={}
     for feature in features:
         out[num_homes][feature] = {}
+        out_mean[num_homes][feature] = {}
         for appliance in appliances:
             out[num_homes][feature][appliance]={}
+            out_mean[num_homes][feature][appliance]={}
             appliance_df = df.ix[all_homes[appliance]]
             if appliance=="hvac":
                 start, stop=5, 11
@@ -42,15 +47,20 @@ for num_homes in range(6, 50, 5):
                 start, stop=1, 13
 
             for home in appliance_df.index.values:
+
                 try:
                     store_path = os.path.expanduser("~/main-out-new-larger-num-homes/%d_%s_%s_%d.csv" %(num_homes, appliance,feature, home))
-                    pred =  pd.read_csv(store_path, index_col=0).squeeze()
+                    store_path_mean = os.path.expanduser("~/main-out-new-larger-num-homes-mean/%d_%s_%s_%d.csv" %(num_homes, appliance,feature, home))
+                    pred_mean = pd.DataFrame({home:pd.read_csv(store_path_mean,index_col=0,squeeze=True).squeeze()}).T
+                    pred = pd.read_csv(store_path, index_col=0).squeeze()
                     gt = appliance_df.ix[home][['%s_%d' %(appliance, month) for month in range(start, stop)]].squeeze()
                     gt.index = pred.index
                     #print gt
                     error = (gt-pred).abs()
+                    error_mean = (gt-pred_mean).abs()
                     #print error
                     percentage_error = error*100/gt
+                    percentage_accuracy_mean=error_mean*100/gt
                     #print percentage_error
                     percentage_error[percentage_error>100]=100
 
@@ -59,5 +69,6 @@ for num_homes in range(6, 50, 5):
                     #print error.mean()
                     #out[num_homes][feature][appliance][home] = error.mean()
                     out[num_homes][feature][appliance][home] = percentage_error.mean()
+                    out_mean[num_homes][feature][appliance][home]=percentage_accuracy_mean.mean()
                 except Exception, e:
                     pass
